060150aac7775b507f6f9ce982b44fe3c3ef9d40
Guillaume Trueb <guillaumetrueb@etik.com>
2024-11-19 20:38:20 +0100

    wip: work on ex13

--- ex13/bridgemanagerfloat.h
+++ ex13/bridgemanagerfloat.h
@@ -5,8 +5,13 @@
 class BridgeManagerFloat
 {
 public:
-    BridgeManagerFloat(float maxWeight)
+    BridgeManagerFloat(float _maxWeight)
     {
+        this->maxWeight = (int)_maxWeight - 1;
+        for(int i = 0 ; i < maxWeight ; ++i)
+        {
+            bridgeControl.release();
+        }
 
     }
 
@@ -17,6 +22,15 @@ public:
 
     void access(float weight)
     {
+        mutex->acquire();
+        if(currentWeight + weight < maxWeight)
+        {
+            for(int i = 0 ; i < weight; ++i)
+                bridgeControl.acquire();
+        } else
+        {
+            bridgeControl.acquire();
+        }
 
     }
 
@@ -24,4 +38,8 @@ public:
     {
 
     }
+protected:
+    PcoSemaphore bridgeControl;
+    PcoSemaphore* mutex = new PcoSemaphore(0);
+    int maxWeight, currentWeight = 0;
 };
